39e821c82f583b3cafdb709d62de784573535f97
Valisher Botirov <valisherbotirov1218@gmail.com>
2022-03-24 12:48:32 +0500

    Vertikal hero sliderni tugatdim

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
 const buttonTepaga = document.querySelector(".btn--tepaga");
 const buttonPastga = document.querySelector(".btn--pastga");
 const imgs = document.querySelector(".right");
+const imgArr = document.querySelectorAll(".img-1");
+const leftDiv = document.querySelector(".left");
+const leftArr = document.querySelectorAll(".box");
 
 let index = 0;
+let indexLeft = leftArr.length - 1;
+function mooveLift() {
+  if (indexLeft < 0) {
+    indexLeft = leftArr.length - 1;
+  } else if (indexLeft > leftArr.length - 1) {
+    indexLeft = 0;
+  }
+  leftDiv.style.transform = `translateY(${-indexLeft * 100}vh)`;
+}
 
 function moove() {
-  imgs.style.transform = `translateY(-${index * 100}vh)`;
-  if (index == 3) {
+  if (index > imgArr.length - 1) {
     index = 0;
+  } else if (index < 0) {
+    index = imgArr.length - 1;
   }
-  // if (index == 0) {
-  //   index = 3;
-  // }
+  imgs.style.transform = `translateY(${-index * 100}vh)`;
 }
+
 buttonTepaga.addEventListener("click", function () {
   index++;
+  indexLeft--;
   moove();
+  mooveLift();
 });
 
 buttonPastga.addEventListener("click", function () {
   index--;
+  indexLeft++;
   moove();
+  mooveLift();
 });
